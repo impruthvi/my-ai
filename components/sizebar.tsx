e723a0c4bcb95af6d4c98e34f0a6e3c8a3346669
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-08-03 23:05:55 +0530

    Implanted a sidebar

---
+++ components/sizebar.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Montserrat } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import {
+  ImageIcon,
+  VideoIcon,
+  Music,
+  Code,
+  Settings,
+  LayoutDashboard,
+  MessageSquare,
+} from "lucide-react";
+
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const routes = [
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    href: "/conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Genration",
+    href: "/image",
+    icon: ImageIcon,
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Genration",
+    href: "/video",
+    icon: VideoIcon,
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Genration",
+    href: "/music",
+    icon: Music,
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Genration",
+    href: "/code",
+    icon: Code,
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: Settings,
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-5">
+            <Image src="/logo.png" alt="Logo" fill />
+          </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            My AI
+          </h1>
+        </Link>
+
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
