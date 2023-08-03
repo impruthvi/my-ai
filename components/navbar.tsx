470df7c3271f9fb50d4f30c01f1fdbd7260cfd43
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-08-03 23:13:52 +0530

    Implemented side bar for mobile screen

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,12 @@
-import { Menu } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
-        <UserButton afterSignOutUrl="/"/>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   );
