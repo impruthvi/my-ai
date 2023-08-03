b0a6dffc42cd77ec8fbf20e38ecd687bf03305e1
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-08-03 22:01:13 +0530

    init commit 🚀🚀

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard (Protected)</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Landing Page(Not Protected)</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
