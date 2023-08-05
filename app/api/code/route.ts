13a97c3765c5069513da963d83be6adfdd75320d
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-08-05 05:40:44 +0530

    implemented a API for generate the code

---
+++ app/api/code/route.ts
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const instructions: ChatCompletionRequestMessage = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code commnets for explaining the code.",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!configuration.apiKey)
+      return new NextResponse("API key now configured", { status: 500 });
+
+    if (!messages) return new NextResponse("Invalid request", { status: 400 });
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [instructions, ...messages],
+    });
+
+    return NextResponse.json(response.data.choices[0].message);
+  } catch (e) {
+    console.log(`[CODE_ERROR] ${e}`);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
