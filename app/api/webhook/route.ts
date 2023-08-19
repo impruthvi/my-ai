05e69492271fc5f5cbbe7aa75d93927a924e3c1d
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-08-19 11:54:04 +0530

    implemented stripe and webook apis

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,69 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import prisma from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("stripe-signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (err) {
+    return new NextResponse("Invalid signature", { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    if (!session?.metadata?.userId) {
+      return new NextResponse("User id is required", { status: 400 });
+    }
+
+    await prisma.userSubscription.create({
+      data: {
+        userId: session?.metadata?.userId,
+        stripeCustomerId: session.id as string,
+        stripeSubscriptionId: subscription.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+        stripePriceId: subscription.items.data[0].price.id,
+      },
+    });
+  }
+
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+    if (!session?.metadata?.userId) {
+      return new NextResponse("User id is required", { status: 400 });
+    }
+    await prisma.userSubscription.update({
+      where: {
+        stripeCustomerId: session.id as string,
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
